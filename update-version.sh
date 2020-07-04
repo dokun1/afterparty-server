d25b89c470be7dd60c7d8d65308e1bb4142b287f
David Okun <david@okun.io>
2020-07-04 15:35:47 -0500

    Now trying to use a subscript

---
+++ update-version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+tagName=${1}
+shaTag="sha-${tagName}"
+
+echo $shaTag
+
+rm -f ./kube/afterparty-deployment.yaml
+
+echo 'apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: afterparty
+spec:
+  selector:
+    matchLabels:
+      app: afterparty
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: afterparty
+    spec:
+      containers:
+      - name: afterparty
+        image: dokun1/afterparty-server-testing:'$shaTag'
+        ports:
+        - containerPort: 8080' > ./kube/afterparty-deployment.yaml
+
